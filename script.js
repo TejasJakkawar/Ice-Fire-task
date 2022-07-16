98f03ccec5ef67c9118d85e4f080aa6bad2a8382
unknown <you@example.com>
2022-07-16 19:43:37 +0530

    Update

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-document.body.innerHTML=
-`<nav class="nav-container">
-<h1>Ice and Fire API<h1></nav>
-<div id="container" class="main-container"></div>`
+var booksContainer=document.getElementById('booksContainer')
+console.log(booksContainer);
+async function getBookData(){
+    let data=await fetch("https://www.anapioficeandfire.com/api/books?page=1&pageSize=20")
+    let res= await data.json()
+    //console.log(res)
+    //showData(res);
 
+    booksContainer.innerHTML = "";
+    res.map((element)=>{
+        console.log(element.name)
+        var bookdiv = document.createElement('div')
+        let bookChar = [];
+        if(element.characters.length>5){
+            let i=0;
 
-async function getData(){
-    try{
-        let response = await fetch("https://www.anapioficeandfire.com/api/books/")
-        let data = await response.json()
-    
-        container.innerHTML=""
-
-        
-        var count = 0; 
-        await data.forEach((dataObj)=>{
-           display(dataObj, count++)
-        })
-        
-    
-        var count1=0
-        await data.forEach((dataObj)=>{
-            charName(dataObj, count1++)
-         })
-    }catch(error) {
-        (error)=>console.log(error);
-    }
-};
-getData()
-
-
-const display = (obj, id)=>{
-    container.innerHTML +=`
-    <div id="childContainer+${id}" class="child-container">
-    <p><b>Name: ${obj.name}</b></p>
-    <p>ISBN: ${obj.isbn}</p>
-    <p>pages: ${obj.numberOfPages}</p>
-    <p>Authors: ${obj.authors}</p>
-    <p>Publisher: ${obj.publisher}</p>
-    <p>Released: ${obj.released}</p>
-    </div>
-    `
+            // FOR PRATIK review
+            do{
+                var req = new XMLHttpRequest();
+                req.open('GET',element.characters[i],false);
+                req.send(null);
+                var response = JSON.parse(req.responseText);
+                //getCharacters(element.characters[i]).then(d => bookChar.push(d))
+                if(response.name !== "") bookChar.push(response.name);
+                i+=1;
+            }while(bookChar.length<=5)
+        }
+         console.log(bookChar);
+        // var itr = bookChar.values();
+        // console.log(itr.next().value);
+        bookdiv.innerHTML=`
+        <div class=" card border-info mb-2" style="max-width: 18rem;">
+            <div class="card-header">
+                <h5> ${element.name}</h5>
+            </div>
+            <div class="card-body m-0 bg-secondary">
+                <p class="card-text m-0">ISBN: ${element.isbn}</p>
+                <p class="card-text m-0">Pages: ${element.numberOfPages}</p>
+                <p class="card-text m-0">Authors: ${element.authors}</p>
+                <p class="card-text m-0">Publisher: ${element.publisher}</p>
+                <p class="card-text m-0">Released: ${element.released}</p>
+                <p class="card-text m-0">Character:${bookChar[0]}</p>
+                <p class="card-text m-0">Character:${bookChar[1]}</p>
+                <p class="card-text m-0">Character:${bookChar[2]}</p>
+                <p class="card-text m-0">Character:${bookChar[3]}</p>
+                <p class="card-text m-0">Character:${bookChar[4]}</p>
+            </div>
+       </div>`
+    booksContainer.append(bookdiv)
+    bookChar = [];
+    })
 }
 
-
-function charName(obj, id1){
-    var charArray = obj.characters.slice(11,16)
-
-    for(let i=0;i<charArray.length;i++){
-        
-        fetch(charArray[i])
-            .then((res)=>res.json())
-            .then((data)=> {
-                document.getElementById("childContainer+"+id1).innerHTML += 
-                `<p id="character">Character: ${data.name}</p>`
-        
-            })
-            .catch((error)=>console.log(error))
-    }
+async function getCharacters(charURL){
+    let charData =await fetch(charURL);
+    let charRes = await charData.json();
+    //console.log(charRes.name);
+    return charRes.name;
 }
+getBookData()
